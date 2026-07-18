f0b9f2fe6b896ec5988e108c9ac437d188afbe9e
xxcode2 <xxcode2@users.noreply.github.com>
2026-07-18 14:46:15 +0000

    Initial commit: X Automation dashboard with Next.js, Tailwind, animations

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    // enable app directory if using Next 14
+  },
+};
+
+module.exports = nextConfig;
